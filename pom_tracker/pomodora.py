b08a63d9c1186557ffedde5e292238c5c39136e2
Arin Blue <arin.a.h.blue@gmail.com>
2018-07-03 17:09:36 -0400

    Implementing SQLite

--- pom_tracker/pomodora.py
+++ pom_tracker/pomodora.py
@@ -1,6 +1,17 @@
 class Pomodora:
 
-    def __init__(self, current_task, label, review):
+    def __init__(self, current_task, flags, review, start_time, end_time):
+        """
+            This class is for a single pomodora
+            It requires:
+                - a current-task in the format of a STRING
+                - a list of flags applied to the pom as a LIST
+                - a review in the format of a string
+                - a start_time
+                - an end_time
+        """
         self.current_task = current_task
-        self.label = label
+        self.flags = flags
         self.review = review
+        self.start_time = start_time
+        self.end_time = end_time
