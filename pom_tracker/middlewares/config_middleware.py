c515787c13c6b5ad67db2c65a826b3c330709660
Arin Blue <arin.a.h.blue@gmail.com>
2018-07-18 09:07:53 -0400

    Added excluding of folders to the config middleware

--- pom_tracker/middlewares/config_middleware.py
+++ pom_tracker/middlewares/config_middleware.py
@@ -6,42 +6,33 @@ class ConfigMiddleware:
         filepath = 'config/pom_sheet_times_template.yaml'
         data = YamlHelper().loader(filepath)
         self.time_blocks = data.get('time_blocks')
-        self.excluded_paths_validate = ['/app/login',
-                                        '/app/logout',
-                                        '/app/create',
-                                        '/app/create_email_exists',
-                                        '/app/home',
-                                        '/app/login_failed',
-                                        '/app/session_expired',
-                                        '/api/users',
-                                        '/favicon.ico',
-                                        '/css/style.css',
-                                        '/css/user_create.css',
-                                        '/css/bootstrap.css',
-                                        '/css/bootstrap.min.css',
-                                        '/css/bootstrap.min.css.map',
-                                        '/css/bootstrap-grid.css',
-                                        '/css/bootstrap-grid.css.map',
-                                        '/css/bootstrap-grid.min.css',
-                                        '/css/bootstrap-grid.min.css.map',
-                                        '/css/bootstrap-reboot.css',
-                                        '/css/bootstrap-reboot.css.map',
-                                        '/css/bootstrap-reboot.min.css',
-                                        '/css/bootstrap-reboot.min.css.map',
-                                        '/js/bootstrap.js',
-                                        '/js/bootstrap.min.js',
-                                        '/js/bootstrap.min.js.map',
-                                        '/js/jquery.js',
-                                        '/assets/time.jpg'
-                                        ]
-        self.included_paths_user = ['/app/pomodora',
-                                    '/api/poms',
-                                    '/api/pom_sheet_export',
-                                    '/app/export_poms',
-                                    '/app/pom_exists'
-                                    ]
+        self.excluded_paths_validate = [
+            '/app/login',
+            '/app/logout',
+            '/app/create',
+            '/app/create_email_exists',
+            '/app/home',
+            '/app/login_failed',
+            '/app/session_expired',
+            '/api/users',
+            '/favicon.ico'
+        ]
+        self.excluded_folders_validate = [
+            '/css/',
+            '/js/',
+            '/assets/'
+        ]
+        self.included_paths_user = [
+            '/app/pomodora',
+            '/api/poms',
+            '/api/pom_sheet_export',
+            '/app/export_poms',
+            '/app/pom_exists'
+        ]
 
     def process_request(self, req, resp):
         req.context['time_blocks'] = self.time_blocks
         req.context['excluded_paths_validate'] = self.excluded_paths_validate
+        req.context[
+            'excluded_folders_validate'] = self.excluded_folders_validate
         req.context['included_paths_user'] = self.included_paths_user
--- pom_tracker/middlewares/validation_middleware.py
+++ pom_tracker/middlewares/validation_middleware.py
@@ -1,5 +1,6 @@
 from helpers.my_requests import Requests
 from resources.session import SessionResource
+import re
 
 
 class ValidationMiddleware:
@@ -7,7 +8,15 @@ class ValidationMiddleware:
         self.session_expire_time = 7200  # 2 hours in seconds
 
     def process_request(self, req, resp):
-        # Check if session exists and is not expired
-        # Simulated downstream request
-        if req.relative_uri not in req.context['excluded_paths_validate']:
-            Requests().get(req, resp, SessionResource())
+        req_uri = req.relative_uri
+        match_folder = False
+        for path in req.context['excluded_folders_validate']:
+            pattern = re.compile(path)
+            if pattern.match(req_uri) is not None:
+                match_folder = True
+
+        if not match_folder:
+            if req_uri not in req.context['excluded_paths_validate']:
+                # Check if session exists and is not expired
+                # Simulated downstream request
+                Requests().get(req, resp, SessionResource())
