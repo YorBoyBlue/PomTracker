b6fd11259c377b807fb58de63e39b57b70351191
Arin Blue <arin.a.h.blue@gmail.com>
2018-07-23 13:20:54 -0400

    Trying to set up setup.py

--- pom_tracker/OldCode/helpers/base.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
-engine = create_engine('sqlite:///databases/pom_tracker.db')
-Base = declarative_base()
--- pom_tracker/OldCode/objects/pomodora.py
+++
@@ -1,7 +0,0 @@
-class Pomodora:
-
-    def __init__(self, time_block, current_task, flags, review):
-        self.current_task = current_task
-        self.flags = flags
-        self.review = review
-        self.time_block = time_block
